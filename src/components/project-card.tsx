bafa0769498a20bdeac83877e055fee4ce96c73c
ShenLongVansh <shenlong.codes@gmail.com>
2025-10-21 12:12:32 +0530

    Made some changes to the github secrets variable

--- src/components/project-card.tsx
+++ src/components/project-card.tsx
@@ -6,6 +6,7 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import React from "react";
 import { cn } from "@/lib/utils";
 import Image from "next/image";
 import Link from "next/link";
